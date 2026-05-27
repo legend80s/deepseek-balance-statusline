91ee216b56e4e3c5058e2d965ca96e2b9b8e203d
legend80s <liuchzong@qq.com>
2026-05-27 22:56:19 +0800

    feat: call get balance api

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-const { BASE_URL, API_KEY } = process.env
+const { BASE_URL = "https://api.deepseek.com", API_KEY } = process.env
 
-console.log("BASE_URL:", BASE_URL)
-console.log("API_KEY:", API_KEY)
+// console.log("BASE_URL:", BASE_URL)
+// console.log("API_KEY:", API_KEY)
 
-async function getBalance(
+export async function getBalance() {
+  const balance = await getBalanceCore()
+
+  return balance?.balance_infos[0].total_balance
+}
+
+async function getBalanceCore(
   opts: { signal?: AbortSignal } = {},
 ): Promise<UserBalance | null> {
   try {
@@ -21,10 +27,19 @@ async function getBalance(
   }
 }
 
+if (import.meta.main) {
+  const balance = await getBalanceCore()
+  console.log("balance:", balance?.balance_infos[0].total_balance)
+}
+
 interface BalanceInfo {
+  /** 货币类型（"CNY" 或 "USD"） */
   currency: string
+  /** 总余额 */
   total_balance: string
+  /** 赠送余额 */
   granted_balance?: string
+  /** 充值余额 */
   topped_up_balance?: string
 }
 
